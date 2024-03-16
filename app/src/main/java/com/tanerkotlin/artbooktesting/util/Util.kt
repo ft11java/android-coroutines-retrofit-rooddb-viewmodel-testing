65ae08f8984fb6472995be7fb98905c5f0fcfcc5
ft11java <ft11java>
2024-03-16 17:33:33 +0300

    2 commit

--- app/src/main/java/com/tanerkotlin/artbooktesting/util/Util.kt
+++ app/src/main/java/com/tanerkotlin/artbooktesting/util/Util.kt
@@ -2,6 +2,6 @@ package com.tanerkotlin.artbooktesting.util
 
 object Util {
 
-    const val API_KEY="**************************"
+    const val API_KEY=""
     const val BASE_URL="https://pixabay.com"
 }
