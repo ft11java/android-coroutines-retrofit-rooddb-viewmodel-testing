4b3d54a74e10f75de62f3e12ab6b611666b9b175
ft11java <ft11java>
2024-03-19 14:25:07 +0300

    Fragment Test Finis

--- app/src/androidTest/java/com/tanerkotlin/artbooktesting/roomdb/ArtDaoTest.kt
+++ app/src/androidTest/java/com/tanerkotlin/artbooktesting/roomdb/ArtDaoTest.kt
@@ -4,28 +4,46 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.tanerkotlin.artbooktesting.getOrAwaitValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
 
 @SmallTest
 @ExperimentalCoroutinesApi
+@HiltAndroidTest
 class ArtDaoTest {
 
     @get :Rule
     var instantTaskExecutorRule=InstantTaskExecutorRule()
 
-    private lateinit var database:ArtDatabase
+    @get:Rule
+    var hiltRule=HiltAndroidRule(this)
+
+    @Inject
+    @Named("testDatabase")
+    lateinit var database: ArtDatabase
+
     private lateinit var dao:ArtDao
 
     @Before
     fun setup(){
+        /* ->>hilt test sürecine katılmadan önce datatbase bu şekilde başlatıldı.
         database=Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),ArtDatabase::class.java
         ).allowMainThreadQueries().build()
+         */
 
+        hiltRule.inject()
         dao=database.artDao()
     }
 
@@ -35,12 +53,20 @@ class ArtDaoTest {
     }
 
     @Test
-    fun insertArtTesting(){
+    fun insertArtTesting()= runTest{
+        val  exampleArt=Art("Mona Lisa","Da Vinci",1700,"test.com",1)
+        dao.insertArt(exampleArt)
+        val list=dao.observeArts().getOrAwaitValue()
+        assertThat(list).contains(exampleArt)
 
     }
     @Test
-    fun deleteArtTesting(){
-
+    fun deleteArtTesting()= runTest{
+        val  exampleArt=Art("Mona Lisa","Da Vinci",1700,"test.com",1)
+        dao.insertArt(exampleArt)
+        dao.deleteArt(exampleArt)
+        val list=dao.observeArts().getOrAwaitValue()
+        assertThat(list).doesNotContain(exampleArt)
     }
 
 
---
+++ app/src/debug/java/com/tanerkotlin/artbooktesting/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.tanerkotlin.artbooktesting
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity:AppCompatActivity() {
+}
